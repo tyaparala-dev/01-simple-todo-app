95d228c064c949be01bd21c09a4ffd5a28fd8f06
unknown <tharunyaparala.dev@gmail.com>
2026-04-15 23:07:54 +0530

    Added most of the back end part

---
+++ src/main/java/com/tharun/simpletodoapp/controller/TaskController.java
@@ -0,0 +1,30 @@
+package com.tharun.simpletodoapp.controller;
+
+import com.tharun.simpletodoapp.dto.TaskRequestDto;
+import com.tharun.simpletodoapp.dto.TaskResponseDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/tasks")
+public interface TaskController {
+
+    @GetMapping
+    List<TaskResponseDto> getAllTasks();
+
+    @GetMapping("/{status}")
+    List<TaskResponseDto> getTasksByStatus(@PathVariable String status);
+
+    @GetMapping("/{id}")
+    TaskResponseDto getTaskById(@PathVariable long id);
+
+    @PostMapping
+    void createTask(@RequestBody TaskRequestDto taskRequestDto);
+
+    @PostMapping("/{id}/status/{status}")
+    void updateTaskStatus(@PathVariable long id, @PathVariable  String status);
+
+    @DeleteMapping
+    void clearAllTasks();
+
+}
---
+++ src/main/java/com/tharun/simpletodoapp/controller/impl/TaskControllerImpl.java
@@ -0,0 +1,49 @@
+package com.tharun.simpletodoapp.controller.impl;
+
+import com.tharun.simpletodoapp.controller.TaskController;
+import com.tharun.simpletodoapp.dto.TaskRequestDto;
+import com.tharun.simpletodoapp.dto.TaskResponseDto;
+import com.tharun.simpletodoapp.entity.TaskStatus;
+import com.tharun.simpletodoapp.svc.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class TaskControllerImpl implements TaskController {
+
+    private final TaskService taskService;
+
+    @Override
+    public List<TaskResponseDto> getAllTasks() {
+        return taskService.getAllTasks().stream().map(taskService::convertToDto).toList();
+    }
+
+    @Override
+    public List<TaskResponseDto> getTasksByStatus(String status) {
+        return taskService.getTasksByTaskStatus(TaskStatus.valueOf(status))
+                .stream().map(taskService::convertToDto).toList();
+    }
+
+    @Override
+    public TaskResponseDto getTaskById(long id) {
+        return taskService.convertToDto(taskService.getTaskById(id));
+    }
+
+    @Override
+    public void createTask(TaskRequestDto taskRequestDto) {
+        taskService.createTask(taskService.convertToDomain(taskRequestDto));
+    }
+
+    @Override
+    public void updateTaskStatus(long id, String status) {
+        taskService.updateTaskStatus(id, TaskStatus.valueOf(status));
+    }
+
+    @Override
+    public void clearAllTasks() {
+        taskService.clearAllTasks();
+    }
+}
